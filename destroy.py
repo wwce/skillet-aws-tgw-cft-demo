03f0e1bdff7614a4daf952c58b7e2b02687cf523
jharris10 <jharris@paloaltonetworks.com>
2019-06-18 13:25:00 +0100

    Added stack status check

--- destroy.py
+++ destroy.py
@@ -14,32 +14,59 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 Region = ''
 
-def delete_stack(stack_name, Region, ACCESS_KEY, SECRET_KEY):
+def delete_stack(stack_name, Region):
     """
 
     :param stack_name:
     :param Region:
     :return:
     """
-    cf_client = boto3.client('cloudformation', 
-                        region_name = Region,
-                        aws_access_key_id=ACCESS_KEY,
-                        aws_secret_access_key=SECRET_KEY)
-
+    cf_client = boto3.client('cloudformation', region_name=Region)
     response = cf_client.delete_stack(StackName=stack_name)
+
     if 'ResponseMetadata' in response and response['ResponseMetadata']['HTTPStatusCode'] < 300:
         logger.info("Got response: {0}".format(response))
-        return True
+
+        while True:
+            try:
+                stack_data = cf_client.describe_stacks(StackName=stack_name)
+                if stack_data['Stacks'][0]['StackStatus'] == 'ROLLBACK_IN_PROGRESS':
+                    print('Stack is rolling back - check the event logs')
+                    time.sleep(30)
+                    continue
+                elif stack_data['Stacks'][0]['StackStatus'] == 'DELETE_IN_PROGRESS':
+                    print('Stack still deleting')
+                    time.sleep(30)
+                    continue
+                elif stack_data['Stacks'][0]['StackStatus'] == 'CREATE_IN_PROGRESS':
+                    print('Stack still creating - check again in 30 secs')
+                    time.sleep(30)
+                    continue
+                elif stack_data['Stacks'][0]['StackStatus'] == 'CREATE_COMPLETE':
+                    print('Stack has deployed successfully')
+                    break
+                elif stack_data['Stacks'][0]['StackStatus'] == 'DELETE_COMPLETE':
+                    print('Stack has been deleted')
+                    break
+                elif stack_data['Stacks'][0]['StackStatus'] == 'DELETE_FAILED':
+                    print('Stack has failed to delete check your console')
+                    break
+                elif stack_data['Stacks'][0]['StackStatus'] == 'ROLLBACK_FAILED':
+                    print('Stack has failed to rollback check your console')
+                    break
+            except ClientError as error:
+                logger.info('Got exception {}'.format(error))
+                break
+            except Exception as e:
+                logger.info('Got exception {}'.format(e))
+                break
+
     else:
         logger.info("There was an Unexpected error. response: {0}".format(response))
         return False
 
-def delete_bucket(s3bucket_name, Region, ACCESS_KEY, SECRET_KEY):
-    s3 = boto3.resource('s3', 
-                        region_name = Region,
-                        aws_access_key_id=ACCESS_KEY,
-                        aws_secret_access_key=SECRET_KEY)
-
+def delete_bucket(s3bucket_name, Region):
+    s3 = boto3.resource('s3', region_name = Region)
     bucket = s3.Bucket(s3bucket_name)
     response = bucket.objects.all().delete()
     bucket.delete()
@@ -54,21 +81,15 @@ def main():
 
     :return:
     """
-
     global Region
     """
     Input arguments
     Mandatory -r Region 'eu-west-1' | 'us-east-1' ......
     """
     parser = argparse.ArgumentParser(description='Get Parameters')
-    parser.add_argument('-k', '--aws_access_key', help='AWS Key', required=True)
-    parser.add_argument('-s', '--aws_secret_key', help='AWS Secret', required=True)
-    parser.add_argument('-r', '--aws_region', help='Select aws_region', default='us-east-1')
+    parser.add_argument('-r', '--Region', help='Select Region', default='us-east-1')
     args = parser.parse_args()
-    
-    ACCESS_KEY = args.aws_access_key
-    SECRET_KEY = args.aws_secret_key
-    aws_region = args.aws_region
+    Region = args.Region
 
     try:
         with open('deployment_data.json', 'r') as data:
@@ -79,17 +100,11 @@ def main():
         logger.infor('Got exception {}'.format(e))
         sys.exit("Could not find deployment config file 'deployment_data.json'")
 
-    if delete_stack(stack_name, aws_region, ACCESS_KEY, SECRET_KEY):
+    # if delete_stack(stack_name, Region):
         print ('Deleted Stack {}'.format(stack_name))
-    else:
-       logger.info('Failed to delete stack') 
 
-    if delete_bucket(s3bucket_name, aws_region, ACCESS_KEY, SECRET_KEY):
+    if delete_bucket(s3bucket_name, Region):
         print ('Deleted S3 Bucket {}'.format(s3bucket_name))
-    else:
-       logger.info('Failed to delete bucket') 
-
-
 
 if __name__ == '__main__':
     main()
